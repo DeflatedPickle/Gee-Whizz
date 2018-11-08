ebe354be234e6517f50401b3e0d4fb0beae5ee01
DeflatedPickle <DeflatedPickle@gmail.com>
2018-11-08 08:02:20 +0000

    Fixed the arms

--- GeeWhizz/scripts/look_at_mouse.gd
+++ GeeWhizz/scripts/look_at_mouse.gd
@@ -1,7 +1,9 @@
 extends Sprite
 
 func _process(delta):
-	var pos = get_viewport().get_mouse_position()
-	var screen_size = get_viewport().get_visible_rect().size
+	# var pos = get_viewport().get_mouse_position()
+	# var screen_size = get_viewport().get_visible_rect().size
 
-	self.look_at(Vector2(screen_size.x - pos.x, screen_size.y - pos.y))
+	# self.look_at(Vector2(screen_size.x - pos.x, screen_size.y - pos.y))
+
+	rotation += deg2rad(fmod(rad2deg(get_local_mouse_position().angle() * 0.8) - (140 * sign(get_local_mouse_position().angle())), 360))
