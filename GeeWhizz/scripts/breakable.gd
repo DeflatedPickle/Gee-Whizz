4b6efb61b454e8f789609095de81fc6a9f3b4994
Dibble <DeflatedPickle@gmail.com>
2018-12-17 11:41:30 +0000

    Fixed errors

--- GeeWhizz/scripts/breakable.gd
+++ GeeWhizz/scripts/breakable.gd
@@ -5,8 +5,8 @@ export(float) var health = 6
 func _process(delta):
 	for b in self.get_colliding_bodies():
 		if "Bullet" in b.name:
-			if self.health - b.parent.bullet_damage > 0:
-				self.health -= b.parent.bullet_damage
+			if self.health - b.parent.get_node("Stats").bullet_damage > 0:
+				self.health -= b.parent.get_node("Stats").bullet_damage
 
 			else:
 				self.queue_free()
